23e13be517226efc083542ab6b8e0bbe655b5fd3
AsmaeAarab <70265206+AsmaeAarab@users.noreply.github.com>
2021-12-16 12:39:19 +0000

    Valider et livrer rapport avec permission

--- src/extensions/validerRapport/ValiderRapportCommandSet.ts
+++ src/extensions/validerRapport/ValiderRapportCommandSet.ts
@@ -30,27 +30,43 @@ const LOG_SOURCE: string = 'ValiderRapportCommandSet';
 export default class ValiderRapportCommandSet extends BaseListViewCommandSet<IValiderRapportCommandSetProperties> {
 
   @override
-  public onInit(): Promise<void> {
-    Log.info(LOG_SOURCE, 'Initialized ValiderRapportCommandSet');
+  public async onInit(): Promise<void> {
+    //Log.info(LOG_SOURCE, 'Initialized ValiderRapportCommandSet');
+    /* test test */
+
+    const def = await sp.web.roleDefinitions.get();
+    
+    //console.log("def", def);
+    // Gets the associated members group of a web
+    /*const memberGroup = await sp.web.associatedMemberGroup();
+    console.log("memberGroup", memberGroup);*/
+    const groups = await sp.web.siteGroups.getByName("Aprés-Validation")();
+    //console.log("groups", groups);
+    const { Id: roleDefId } = await sp.web.roleDefinitions.getByName("Autorisation aprés validation rapport").get();
+    
+    //console.log("roleDefId", roleDefId);
+
+    /* test test */
     return Promise.resolve();
   }
 
   @override
   public onListViewUpdated(event: IListViewCommandSetListViewUpdatedParameters) {
+    
     const compareOneCommand: Command = this.tryGetCommand('COMMAND_1');
+    const compareOneTwo: Command = this.tryGetCommand('COMMAND_2');
     Libraryurl = this.context.pageContext.list.title;
-        
-    console.log("Libraryurl", Libraryurl);
     if (compareOneCommand) {
       // This command should be hidden unless exactly one row is selected.
       compareOneCommand.visible = event.selectedRows.length === 1 && (event.selectedRows[0].getValueByName("statut_rapport") === "Traité à valider" && (Libraryurl === "Grands Projets 2022" || Libraryurl === "Rapports 2022"));
+      compareOneTwo.visible = event.selectedRows.length === 1 && (event.selectedRows[0].getValueByName("statut_rapport") === "Validé à livrer" && (Libraryurl === "Grands Projets 2022" || Libraryurl === "Rapports 2022"));
     }
   }
 
   @override
   public async onExecute(event: IListViewCommandSetExecuteEventParameters): Promise<void> {
     let id_rapport:number = event.selectedRows[0].getValueByName("ID");
-    console.log("rapport: ", event.selectedRows[0]);
+    //console.log("rapport: ", event.selectedRows[0]);
     let FileRef = event.selectedRows[0].getValueByName("FileRef");
     
     userEmail = this.context.pageContext.user.email;
@@ -61,19 +77,32 @@ export default class ValiderRapportCommandSet extends BaseListViewCommandSet<IVa
       return element.membre_refId === userId;
     };
     const isValidateur = validateur.filter(query).length === 0? false: true;
-    console.log("validateur", isValidateur);
+    const isLivreur = validateur.filter(query).length === 0? false: true;
+    //console.log("validateur", isValidateur);
+    const confirmationDialog: ConfirmationDialog = new ConfirmationDialog();
     switch (event.itemId) {
       case 'COMMAND_1':
-        const FileLeafRef = event.selectedRows[0].getValueByName("FileLeafRef");
-        const confirmationDialog: ConfirmationDialog = new ConfirmationDialog();
         confirmationDialog.userEmail=userEmail;
         confirmationDialog.Libraryurl=Libraryurl;
         confirmationDialog.id_rapport=id_rapport;
         confirmationDialog.FileRef=FileRef;
+        confirmationDialog.statut="Validé";
         if(isValidateur)
           confirmationDialog.show();
         else
-          Dialog.alert(`Vous n'êtes pas autorisé à effectuer cette action .`);
+          Dialog.alert(`Vous n'êtes pas autorisé à effectuer cette action.`);
+        break;
+      
+      case 'COMMAND_2':
+        confirmationDialog.userEmail=userEmail;
+        confirmationDialog.Libraryurl=Libraryurl;
+        confirmationDialog.id_rapport=id_rapport;
+        confirmationDialog.FileRef=FileRef;
+        confirmationDialog.statut="Livré";
+        if(isLivreur)
+          confirmationDialog.show();
+        else
+          Dialog.alert(`Vous n'êtes pas autorisé à effectuer cette action.`);
         break;
       default:
         throw new Error('Unknown command');
--- src/extensions/validerRapport/components/ConfirmationDialog.tsx
+++ src/extensions/validerRapport/components/ConfirmationDialog.tsx
@@ -12,6 +12,7 @@ export default class ConfirmationDialog extends BaseDialog {
     public id_rapport:number;
     public FileRef:string;
     public Libraryurl:string;
+    public statut:string;
 
     public render(): void {
         ReactDOM.render(<ConfirmationDialogContent
@@ -36,34 +37,50 @@ export default class ConfirmationDialog extends BaseDialog {
     
     private _submit = async () => {
         this.close();
-        await this.validerRapport(this.Libraryurl,this.userEmail, this.id_rapport, this.FileRef);
+        if(this.statut=="Validé")
+            await this.validerRapport(this.Libraryurl,this.userEmail, this.id_rapport, this.FileRef);
+        else{
+            if(this.statut=="Livré"){
+                this.message='livré'
+            await this.livrerRapport(this.Libraryurl,this.userEmail, this.id_rapport, this.FileRef);
+            }
+        }
     }
 
     private async validerRapport(Libraryurl:string, userEmail:string, id_rapport:number, folderRacine:string){
         
-        console.log("userId", await getUser(userEmail));
+        //console.log("userId", await getUser(userEmail));
         var userId = await (await getUser(userEmail)).data.Id;
         //var date = new Date().toLocaleString("en-US", {timeZone: "Africa/Casablanca"});
         var _date = new Date().toLocaleString();
-        console.log("_date", _date);
+        //console.log("_date", _date);
         sp.web.lists.getByTitle(Libraryurl).items.getById(id_rapport).update({
             date_x0020_de_x0020_validation: _date
         }).then(async res=>{
             const itemlool = await res.item.get()
-            console.log("date_x0020_de_x0020_validation", itemlool);
+            //console.log("date_x0020_de_x0020_validation", itemlool);
         })
         let item = await sp.web.lists.getByTitle(Libraryurl).items.getById(id_rapport).update({
           statut_rapport: "Validé à livrer",
           validateur_refId: userId,
           date_x0020_de_x0020_validation: _date
         });
-        /*const folder = sp.web.getFolderByServerRelativePath(folderRacine);////"+FileLeafRef);
+        const folder = sp.web.getFolderByServerRelativePath(folderRacine);////"+FileLeafRef);
         const folderItem = await folder.getItem();
         await folderItem.breakRoleInheritance(false);
-        const { Id: roleDefId } = await sp.web.roleDefinitions.getByName('Read').get();
+        //const { Id: roleDefId } = await sp.web.roleDefinitions.getById(1073741928).get();
+        
+        const { Id: roleDefId } = await sp.web.roleDefinitions.getByName("Gestion").get();
+        //console.log("Gestion", roleDefId);
+        const { Id: roleDefI2 } = await sp.web.roleDefinitions.getByName("Collaboration").get();
+        //console.log("Direction", roleDefI2);
+
         //id=5 for members
-        await folderItem.roleAssignments.add(5, roleDefId);
-        const roles = await folderItem.roleAssignments.get();
+        const groups = await sp.web.siteGroups.getByName("Gestion")();
+        const groups2 = await sp.web.siteGroups.getByName("Direction")();
+        await folderItem.roleAssignments.add(groups.Id, roleDefId);
+        await folderItem.roleAssignments.add(groups2.Id, roleDefI2);
+        /*const roles = await folderItem.roleAssignments.get();
         console.log("folder roles", roles);
     
         const obj = await sp.web.firstUniqueAncestorSecurableObject.get();
@@ -72,11 +89,27 @@ export default class ConfirmationDialog extends BaseDialog {
         console.log("getCurrentUserEffectivePermissions", perms2);
         const groups = await sp.web.siteGroups();
         console.log("groups", groups);
-        const def = await sp.web.roleDefinitions.get();
-        
-        console.log("def", def);*/
-        // Gets the associated members group of a web
-        const memberGroup = await sp.web.associatedMemberGroup();
-        console.log("memberGroup", memberGroup);
-      }
+        */
+    }
+
+    private async livrerRapport(Libraryurl:string, userEmail:string, id_rapport:number, folderRacine:string){
+        var userId = await (await getUser(userEmail)).data.Id;
+        var _date = new Date().toLocaleString();
+        sp.web.lists.getByTitle(Libraryurl).items.getById(id_rapport).update({
+            date_x0020_de_x0020_validation: _date
+        }).then(async res=>{
+            const itemlool = await res.item.get()
+        })
+        let item = await sp.web.lists.getByTitle(Libraryurl).items.getById(id_rapport).update({
+          statut_rapport: "Livré",
+          validateur_refId: userId,
+          date_x0020_de_x0020_validation: _date
+        });
+        const folder = sp.web.getFolderByServerRelativePath(folderRacine);////"+FileLeafRef);
+        const folderItem = await folder.getItem();
+        //await folderItem.breakRoleInheritance(false);
+        const { Id: roleDefId } = await sp.web.roleDefinitions.getByName("Gestion").get();
+        const groups = await sp.web.siteGroups.getByName("Gestion")();
+        await folderItem.roleAssignments.remove(groups.Id, roleDefId);
+    }
 }
